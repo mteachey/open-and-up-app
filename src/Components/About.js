d9bfc0392b7497e7f27cdf3d39280b1916ffe839
mteachey <melindateachey@gmail.com>
2020-07-29 12:34:48 -0600

    fixed a few typos

--- src/Components/About.js
+++ src/Components/About.js
@@ -9,7 +9,7 @@ class About extends Component{
                     <h2>What is Uplift?</h2>
                     <p>In a world, where we are so often bombarded with negative news or feeds that cause disagreement, envy, and anxiety, this app was created to be a place to find positive, uplifting thoughts, books, podcasts, and events.</p>
                     <p>You can create your own posts, follow other users, and bookmark posts - you own and other users.  In addition, you can write personal, private (only you will see them) comments on any of your bookmarked posts.</p>
-                    <p>There are no likes or dislikes, other users will not know if anyone has or has not bookmarked their posts or chose to follow them.</p>
+                    <p>There are no likes or dislikes, other users will not know if anyone has or has not bookmarked their posts or chosen to follow them.</p>
                     <p>We hope this app can uplift your life....even if just a little.</p>
                 </section>
                 <LearnMoreContent/>
